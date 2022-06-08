f8e10b1f63e7b9940f85784faa81a99a30ab372a
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-08 16:57:43 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -27,7 +27,7 @@ st.subheader('User Input parameters')
 st.write(df)
 
 iris = pd.read_csv("https://raw.githubusercontent.com/NhudaAziz/practice/main/IRIS.csv")
-X = iris[data]
+X = iris['sepal_length','sepal_width','petal_length','petal_width']
 Y = iris.species
 
 clf = RandomForestClassifier()

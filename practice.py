2105f4ec8f00b6ab19d801dd5e1cfc4e42394e35
Nurul Huda Ab Aziz <106502722+NhudaAziz@users.noreply.github.com>
2022-06-08 17:16:29 +0800

    Update practice.py

--- practice.py
+++ practice.py
@@ -38,8 +38,8 @@ prediction = clf.predict(df)
 prediction_proba = clf.predict_proba(df)
 
 st.subheader('Class labels and their corresponding index number')
-#y_label = ['setosa','versicolor','virginica']
-st.write(iris.species['setosa','versicolor','virginica'])
+y_label = ['setosa','versicolor','virginica']
+st.write(iris.species[y_label])
 
 st.subheader('Prediction')
 #st.write(iris.target_names[prediction])
